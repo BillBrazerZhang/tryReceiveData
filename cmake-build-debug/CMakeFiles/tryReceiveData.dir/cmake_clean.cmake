b86c209fb88b3431fc168ff46a75fde54342aed0
WenyuZhang1995 <wez078@ucsd.edu>
2019-11-13 19:19:28 -0800

    first try

---
+++ cmake-build-debug/CMakeFiles/tryReceiveData.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tryReceiveData.dir/main.cpp.o"
+  "tryReceiveData"
+  "tryReceiveData.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tryReceiveData.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
